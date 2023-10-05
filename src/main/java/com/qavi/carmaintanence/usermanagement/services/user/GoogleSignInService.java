dd51a00b09130b318ac26f6ef43867d49fa2b600
syedmisbahali1111-admin <syedmisbahali1111@gmail.com>
2023-10-05 07:38:37 -0700

    access token bug fixed

--- src/main/java/com/qavi/carmaintanence/usermanagement/services/user/GoogleSignInService.java
+++ src/main/java/com/qavi/carmaintanence/usermanagement/services/user/GoogleSignInService.java
@@ -1,35 +1,34 @@
 package com.qavi.carmaintanence.usermanagement.services.user;
 
-import com.qavi.carmaintanence.globalexceptions.InvalidTokenException;
-import com.qavi.carmaintanence.globalexceptions.RecordAlreadyExists;
 import com.qavi.carmaintanence.usermanagement.constants.UserConstants;
 import com.qavi.carmaintanence.usermanagement.entities.user.User;
-import com.qavi.carmaintanence.usermanagement.models.GoogleResponseModel;
 import com.qavi.carmaintanence.usermanagement.models.UserDataModel;
+import com.qavi.carmaintanence.usermanagement.repositories.RoleRepository;
 import com.qavi.carmaintanence.usermanagement.repositories.UserRepository;
 import com.qavi.carmaintanence.usermanagement.utils.JWTGenerator;
 import okhttp3.OkHttpClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class GoogleSignInService {
 
     private final OkHttpClient apiClient;
     private final UserService userService;
+
+    private final RoleRepository roleRepository;
     private final JWTGenerator jwtGenerator;
 
     private final UserRepository appUserRepository;
 
 
-    public GoogleSignInService(UserService userService, JWTGenerator jwtGenerator, UserRepository appUserRepository) {
+    public GoogleSignInService(UserService userService, RoleRepository roleRepository, JWTGenerator jwtGenerator, UserRepository appUserRepository) {
 
         this.userService = userService;
+        this.roleRepository = roleRepository;
         this.jwtGenerator = jwtGenerator;
         this.appUserRepository = appUserRepository;
         this.apiClient = new OkHttpClient.Builder()
@@ -62,6 +61,7 @@ public class GoogleSignInService {
             newUser.setFirstName(userDataModel.getFirstName()); // Use values from userDataModel
             newUser.setLastName(userDataModel.getLastName());   // Use values from userDataModel
             newUser.setEnabled(true);
+            newUser.setRole(Set.of(roleRepository.searchByName("ROLE_OWNER"), roleRepository.searchByName("ROLE_CUSTOMER")));
             newUser.setEmail(userDataModel.getEmail());        // Use values from userDataModel
             newUser.setAuthType(UserConstants.GOOGLE);
             newUser.setEmailNotificationEnabled(false);
--- src/main/java/com/qavi/carmaintanence/usermanagement/utils/JWTGenerator.java
+++ src/main/java/com/qavi/carmaintanence/usermanagement/utils/JWTGenerator.java
@@ -2,7 +2,10 @@ package com.qavi.carmaintanence.usermanagement.utils;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.qavi.carmaintanence.usermanagement.entities.permission.PermissionAssigned;
+import com.qavi.carmaintanence.usermanagement.entities.role.Role;
 import com.qavi.carmaintanence.usermanagement.entities.user.User;
+import com.qavi.carmaintanence.usermanagement.models.PermissionBitsModel;
 import com.qavi.carmaintanence.usermanagement.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.qavi.carmaintanence.CarmaintanenceApplication.loginExpiryTimeMinutes;
@@ -29,13 +35,32 @@ public class JWTGenerator {
 
     public String generateJWTToken(User user){
         Algorithm algorithm = Algorithm.HMAC256(secretJWT.getBytes());
+
         CustomUserDetails customUserDetails=userService.loadUserByUsername(user.getEmail());
+        Collection<Role> roles = customUserDetails.getRoles();
+        List<PermissionBitsModel> permissionBitsModelList = new ArrayList<>();
+        List<String> newRoles = new ArrayList<>();
+        for (Role role : roles) {
+            newRoles.add(role.getName());
+            Collection<PermissionAssigned> fetchedPermission = role.getPermissionAssigned();
+            for (PermissionAssigned permission : fetchedPermission) {
+                if (!permissionBitsModelList.contains(permission.getPermission().getName())) {
+                    PermissionBitsModel permissionBitsModel = new PermissionBitsModel();
+                    permissionBitsModel.setPermission(permission.getPermission().getName());
+                    permissionBitsModel.setPermissionBit(permission.getPermissionBits());
+                    permissionBitsModelList.add(permissionBitsModel);
+                }
+            }
+        }
         String accessToken = JWT.create()
                 .withSubject(String.valueOf(user.getId()))
                 .withExpiresAt(java.sql.Timestamp.valueOf(LocalDateTime.now().plusMinutes(loginExpiryTimeMinutes)))
                 .withIssuer(request.getRequestURL().toString())
-                .withClaim("roles",customUserDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .withClaim("role", newRoles)
+                .withClaim("permissions", permissionBitsModelList.stream().map(p -> p.getPermission()).collect(Collectors.toList()))
+                .withClaim("permissionBits", permissionBitsModelList.stream().map(p -> p.getPermissionBit()).collect(Collectors.toList()))
                 .sign(algorithm);
+
         return accessToken;
     }
 }
